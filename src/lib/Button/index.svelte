09f7c71d0f558ba5b4f579ea124af62b335c20ef
Cindy Kee <cindy@thekees.com>
2021-06-05 20:59:49 -0400

    SCSS is working globally and locally if I import variables all places.

---
+++ src/lib/Button/index.svelte
@@ -0,0 +1,12 @@
+<button>Click me!</button>
+
+<!-- This works by itself -->
+<style lang="scss">
+  @import 'src/styles/variables/index.scss';
+
+  button {
+    color: $primary;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,32 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
+import scss from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess(),
+		//preprocess({
+			// defaults: {
+			// 	style: 'scss'
+			// },
+			// scss: {
+			// 	// We can use a path relative to the root because
+			// 	// svelte-preprocess automatically adds it to `includePaths`
+			// 	// if none is defined.
+			// 	// This allows us to use the variables in our components
+			// 	prependData: `@import '/src/styles/variables/index.scss';`,
+			// 	outputStyle: 'compressed',
+			// }
+		//}),
+		scss(),
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
-};
+}
 
-export default config;
+export default config
