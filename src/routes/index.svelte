5c64f9fefe87005924adf126730441c3d3e2b9fc
Cindy Kee <cindy@thekees.com>
2021-06-06 14:02:56 -0400

    Setup layout and index page with Bootstrap & Christmas banner.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
-<script>
-  import Button from '$lib/Button/index.svelte'
-</script>
+<svelte:head>
+	<title>Wishlist Home</title>
+</svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Button />
+<p class="fs-3">Index page!</p>
+<p class="fs-5">Font Size 5!</p>
+<p class="fs-6">Font Size 6!</p>
+<p>Default P font size!</p>
