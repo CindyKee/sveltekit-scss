4b23bf94cc631ab71d2b6446bc7105c5d9ce376e
Cindy Kee <cindy@thekees.com>
2021-06-12 00:44:19 -0400

    Add an image to replace the Bootstrap logo

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -2,7 +2,7 @@
 
 <div class="hero-bg px-4 py-5 my-5 text-center">
   <!-- <img class="d-block mx-auto mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" /> -->
-  <img class="d-block mx-auto mb-4" src="" alt="" width="72" height="57" />
+  <img class="d-block mx-auto mb-4" src="./images/903px-Christmas_tree_02.svg.png" alt="" width="88" height="100" />
   <h1 class="display-5 fw-bold">Centered hero</h1>
   <div class="col-lg-6 mx-auto">
     <p class="lead mb-4">
